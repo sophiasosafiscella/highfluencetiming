88556d7d2422ad11630ca3b32098889465b71acb
sophiasosafiscella <sophia.sosa@nanograv.org>
2023-12-06 00:06:31 -0500

    Added rficlean.py

--- rficlean.py
+++ rficlean.py
@@ -6,11 +6,15 @@ from tqdm import tqdm
 
 F0 : float = 62.2958887973344346
 psrfbins : int = 32
-files = glob.glob("./*fits")
+fits_files = glob.glob("./*fits")
 
-out_folder = "./rficleaned"
-if not os.path.isdir(out_folder):
-	os.makedirs(out_folder)
+fil_files = fits_files
+for n, file in enumerate(fil_files):
+	fil_files[n] = file[:-5] + ".fil"
 
-for file in tqdm(files):
-	subprocess.run("./rficlean " + file + " -o " + out_folder + file[1:-3] + "_rficleaned.fits " + "-psrf " + str(F0) + " -psrfbins " + str(psrfbins))
+# Convert the .fits files to SIGPROC filterbank format files
+subprocess.run("digifil -b 8 -d 1 -cont -o" + ' '.join(fil_files) + " " + ' '.join(fits_files))
+
+for file in tqdm(fil_files):
+
+	subprocess.run("./rficlean " + file + " -o " + file[:-5] + "_rficleaned.fits " + "-psrf " + str(F0) + " -psrfbins " + str(psrfbins))
