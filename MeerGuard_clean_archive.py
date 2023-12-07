375350681f71e1dd86813b61c920653a7e6275bd
sophiasosafiscella <sophia.sosa@nanograv.org>
2023-12-07 15:53:11 -0500

    Included coast_guard

--- MeerGuard_clean_archive.py
+++ MeerGuard_clean_archive.py
@@ -5,7 +5,7 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
 # Import CoastGuard
-import cleaners
+from coast_guard import cleaners
 import argparse
 import psrchive as ps
 import os
