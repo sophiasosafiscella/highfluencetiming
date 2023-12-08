b4886024dd4fd680a4dee60a74a93e52f0ac8b27
Sophia Valentina Sosa Fiscella <sophia.sosa@nanograv.org>
2023-12-08 11:42:36 -0500

    Ran restricted BW again

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     #   0) Get the file names
     band: str = "820_band"
     classifier: str = "Kmeans"        # Options: "Kmeans", "MeanShift", or "AffinityPropagation"
-    results_dir: str = "./results/" + band + "_new_full_bw/"  # Directory with the results
+    results_dir: str = "./results/" + band + "_new_restricted_bw/"  # Directory with the results
     pulses_dir: str = "./data/" + band + "/"
 
     if band == "L_band":
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     meerguard_clean: bool = False  # Clean using MeerGuard?
 
     binary_out_dir: str = pulses_dir + "binary/"
-#    bandpass = [16, 8]                             # How many channels we're removing from the upper and lower edges
+    bandpass = [16, 8]                             # How many channels we're removing from the upper and lower edges
     times_file: str = binary_out_dir + "times_data.npy"
     channels_file: str = binary_out_dir + "channels_data.npy"
 
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     #   4) Convert the observations to binary and calculate the off-pulse noise RMS
     if len(glob.glob(binary_out_dir + "GUPPI*npy")) < len(files):
         print("Converting the observation to binary files...")
-        times_data, channels_data, rms_array = to_binary_and_calculate_rms(files, binary_out_dir, sp_total)
+        times_data, channels_data, rms_array = to_binary_and_calculate_rms(files, binary_out_dir, sp_total, bandpass)
         np.save(times_file, times_data)
         np.save(channels_file, channels_data)
         np.save(rms_data_file, rms_array)
