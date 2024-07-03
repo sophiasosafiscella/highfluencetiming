ee30f28e4a058c233bdfe138de7fe02b1ee055f7
sophiasosafiscella <sophia.sosa@nanograv.org>
2024-07-03 18:24:45 -0400

    Newest version.

--- sp_utils.py
+++ sp_utils.py
@@ -309,31 +309,28 @@ def estimate_peak(window_data, windows, baseline, window_index, plot=False):
 #    peak_idx = [np.argmax(new_y)]
 
     peaks, _ = find_peaks(new_y)
-    width = peak_widths(new_y, peaks, rel_height=0.5)
-    peak_width = width[0][0]
+    results_half = peak_widths(new_y, peaks, rel_height=0.5)
+    peak_width = results_half[0][0]
 #    peak_width = 2 * result.params["stddev"].value * np.sqrt(2 * np.log(2))
 
     if plot:
         sns.set_style("darkgrid")
         sns.set_context("paper", font_scale=1.4)
 
-        fig, ax = plt.subplots()
-        ax.plot(new_x[peaks], new_y[peaks], "x")
-        ax.plot(x_data, window_data, c="#636EFA", label="Original")  # c="#f29ad8"
-        ax.scatter(x_data, window_data, c="#636EFA")  #  c="#f29ad8"
-        ax.plot(new_x, new_y, c="#EF553B", label="Fit")  # c="#e305ad",
+        plt.plot(new_x[peaks], new_y[peaks], "x")
+#        plt.plot(x_data, window_data, c="#636EFA", label="Original")  # c="#f29ad8"
+#        plt.scatter(x_data, window_data, c="#636EFA")  #  c="#f29ad8"
+        plt.plot(new_x, new_y, c="#EF553B", label="Fit")  # c="#e305ad",
 
-        ax.axvline(x=peak_pos, ls="--", c='k', label="Peak position")
-        ax.axvline(x=windows[1, 0], ls=":", c="grey")
-        ax.axvline(x=windows[1, 1], ls=":", c="grey")
+#        plt.vline(x=peak_pos, ls="--", c='k', label="Peak position")
+        plt.axvline(x=windows[1, 0], ls=":", c="grey")
+        plt.axvline(x=windows[1, 1], ls=":", c="grey")
 #        ax.fill_between(new_x, 0, peak_amp,
 #                        where=(new_x < peak_pos + peak_width/20) &
 #                              (new_x > peak_pos - peak_width/20),
 #                        color='#B6E880', alpha=0.3, label="Width")  # color="#f9dd9a",
 
-        print(*width[1:])
-        sys.exit()
-        ax.axhline(*width[1:], color='C1')
+        plt.hlines(*results_half[1:], color="C2")
 
 #        textstr = '\n'.join((
 #            r'$\mathrm{Position}=%i$' % (peak_pos,),
@@ -348,8 +345,8 @@ def estimate_peak(window_data, windows, baseline, window_index, plot=False):
 #        ax.text(0.70, 0.95, textstr, transform=ax.transAxes, fontsize=12,
 #                verticalalignment='top', bbox=props)
 
-        ax.set_xlabel("Bins")
-        ax.set_ylabel("Intensity")
+        plt.xlabel("Bins")
+        plt.ylabel("Intensity")
         #        ax.set_title("Pulse " + str(window_index))
         plt.legend(loc='upper left')
 
